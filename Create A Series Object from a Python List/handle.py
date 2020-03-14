931a844a5f9b8b9f43f2be0e37af3ff34dc818d9
vudinhquang <vudinhquangk53@gmail.com>
2020-03-14 17:50:31 +0900

    Create A Series Object from a Python List

---
+++ Create A Series Object from a Python List/handle.py
@@ -0,0 +1,9 @@
+import pandas as pd
+ice_cream = ["Chocolate", "Vanilla", "Strawberry", "Run Raisin"]
+print(pd.Series(ice_cream))
+
+lottery=[4,8,15,16,23,42]
+print(pd.Series(lottery))
+
+registrations=[True, False, False, False]
+print(pd.Series(registrations))
