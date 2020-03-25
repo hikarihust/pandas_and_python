f42fa4137d9faeb36d28c2e2c1ad7f463571018b
vudinhquang <vudinhquangk53@gmail.com>
2020-03-26 00:51:39 +0900

    Start Merging, Joining, and Concatenating

---
+++ Merging Joining and Concatenating/Start/handle.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+week1 = pd.read_csv("../Restaurant - Week 1 Sales.csv")
+week2 = pd.read_csv("../Restaurant - Week 2 Sales.csv")
+customers = pd.read_csv("../Restaurant - Customers.csv")
+foods = pd.read_csv("../Restaurant - Foods.csv")
